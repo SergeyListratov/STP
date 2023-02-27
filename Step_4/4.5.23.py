302430485af6427d440ce979d0fd7cfdc61dab94
sergey <sergey.listratov@gmail.com>
2023-02-27 21:46:23 +0300

    Home 4.5.23 01

--- Step_4/4.5.23.py
+++ Step_4/4.5.23.py
@@ -6,8 +6,12 @@ def foo():
 
 
 if __name__ == '__main__':
-    info_list = []
+    info_list, list_set = [], {'', 0}
     with ZipFile('test.zip', 'r') as zip_f:
         info_l = [i.filename.split('/') + [i.file_size] for i in zip_f.infolist()]
     print(info_l)
     for i in info_l:
+        for j in range(len(i)):
+            if i[j] not in list_set:
+                print(f'{" " * 2 * j}{i[j]}')
+                list_set.add(i[j])
