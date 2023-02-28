9ac9483ac4ac5eaacd454c5ccd5ecadac21e5d37
Sergey <sergey.listratov@gmail.com>
2023-02-28 08:16:57 +0300

    rpz 28_02_02 01

--- Step_4/4.5.23.py
+++ Step_4/4.5.23.py
@@ -1,7 +1,8 @@
 from zipfile import ZipFile
 import json
 
-def foo():
+
+def data_v(dat):
     pass
 
 
@@ -11,7 +12,10 @@ if __name__ == '__main__':
         info_l = [i.filename.split('/') + [i.file_size] for i in zip_f.infolist()]
     print(info_l)
     for i in info_l:
-        for j in range(len(i)):
+        for j in range(len(i) - 1):
             if i[j] not in list_set:
-                print(f'{" " * 2 * j}{i[j]}')
+                if i[j + 1] != 0 and type(i[j + 1]) == int:
+                    print(f'{" " * 2 * j}{i[j]} {i[j + 1]}')
+                else:
+                    print(f'{" " * 2 * j}{i[j]}')
                 list_set.add(i[j])
