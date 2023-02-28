e735f8fbfac503d397aa8fe6002a9578d35e7454
Sergey <sergey.listratov@gmail.com>
2023-02-28 09:49:22 +0300

    rpz 28_02_02 01

--- Step_4/4.5.23.py
+++ Step_4/4.5.23.py
@@ -2,20 +2,27 @@ from zipfile import ZipFile
 import json
 
 
-def data_v(dat):
-    pass
+def data_v(size: int):
+    if size < 1024:
+        return f'{size} {"B"}'
+    elif 1024 <= size < 1048576:
+        return f'{round(size / 1024)} {"KB"}'
+    elif 1048576 <= size < 1073741824:
+        return f'{round(size / (1024 * 1024))} {"MB"}'
+    elif 1073741824 <= size < 1073741824:
+        d = round(size / (1024 * 1024 * 1024))
+        return f'{round(size / (1024 * 1024 * 1024))} {"GB"}'
 
 
 if __name__ == '__main__':
     info_list, list_set = [], {'', 0}
-    with ZipFile('test.zip', 'r') as zip_f:
+    with ZipFile('desktop.zip', 'r') as zip_f:
         info_l = [i.filename.split('/') + [i.file_size] for i in zip_f.infolist()]
-    print(info_l)
     for i in info_l:
         for j in range(len(i) - 1):
             if i[j] not in list_set:
                 if i[j + 1] != 0 and type(i[j + 1]) == int:
-                    print(f'{" " * 2 * j}{i[j]} {i[j + 1]}')
+                    print(f'{" " * 2 * j}{i[j]} {data_v(i[j + 1])}')
                 else:
                     print(f'{" " * 2 * j}{i[j]}')
                 list_set.add(i[j])
