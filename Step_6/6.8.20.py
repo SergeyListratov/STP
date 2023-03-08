a79fe81b6ec267df94cd3cd14e946b55b03e9854
sergey <sergey.listratov@gmail.com>
2023-03-08 21:44:07 +0300

    Home 8.03.23 6.6.20 02

---
+++ Step_6/6.8.20.py
@@ -0,0 +1,18 @@
+from collections import Counter
+
+
+def min_values():
+    pass
+
+
+def max_values():
+    pass
+
+
+if __name__ == '__main__':
+    # s_dict = or
+    with open('name_log.csv', 'r', encoding='UTF-8') as file:
+        title, *lst = file.read().split('\n')
+    # s = Counter()
+
+        print(lst)
