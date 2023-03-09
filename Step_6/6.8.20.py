dc5eaf11c56dbce013c2fcd27e5819977be87530
Sergey <sergey.listratov@gmail.com>
2023-03-09 16:33:07 +0300

    rpz 09_01_23 01

--- Step_6/6.8.20.py
+++ Step_6/6.8.20.py
@@ -1,21 +1,13 @@
 from collections import Counter
 
+books = Counter(map(int, input().split()))
+total = 0
 
-if __name__ == '__main__':
-    c_dict, total = dict(), 0
-    b_dict = Counter(input().split())
-    print(b_dict)
-    for i in range(int(input())):
-        b = input().split()
-        if b[0] in c_dict:
-            c_dict[b[0]].append(int(b[1]))
-        else:
-            c_dict[b[0]] = [int(b[1])]
-    for k, v in b_dict.items():
-        if k in c_dict:
-            if len(c_dict[k]) >= v:
-                total += sum(c_dict[k][:v])
-            else:
-                total += sum(c_dict[k][:len(c_dict[k])])
+for _ in range(int(input())):
+    book, price = map(int, input().split())
+    total += bool(books[book]) * price
+    books -= Counter({book: 1})
 
-    print(total)
+print(total)
+
+
