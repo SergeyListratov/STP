a1a6bfffc4c11ed0697c52200ab1dea8e1f06630
Sergey <sergey.listratov@gmail.com>
2023-03-09 16:26:26 +0300

    rpz 09_01_23 01

--- Step_6/6.8.20.py
+++ Step_6/6.8.20.py
@@ -1,9 +1,21 @@
 from collections import Counter
 
 
-def scrabble(symbols: str, word: str) -> bool:
-    pass
-
-
 if __name__ == '__main__':
-    print(scrabble('bbbbbeeeeegggggggeeeeeekkkkk', 'Beegeek'))
+    c_dict, total = dict(), 0
+    b_dict = Counter(input().split())
+    print(b_dict)
+    for i in range(int(input())):
+        b = input().split()
+        if b[0] in c_dict:
+            c_dict[b[0]].append(int(b[1]))
+        else:
+            c_dict[b[0]] = [int(b[1])]
+    for k, v in b_dict.items():
+        if k in c_dict:
+            if len(c_dict[k]) >= v:
+                total += sum(c_dict[k][:v])
+            else:
+                total += sum(c_dict[k][:len(c_dict[k])])
+
+    print(total)
